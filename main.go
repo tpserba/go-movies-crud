93049b1ae61c168e4486410d88bd175ed851f283
Tomás Praga <tpraga@serbatic.es>
2023-04-13 16:39:06 +0200

    add update function implementation

--- main.go
+++ main.go
@@ -60,12 +60,25 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
 	var movie Movie
 	_ = json.NewDecoder(r.Body).Decode(&movie)
+	fmt.Println(&movie)
 	movie.Id = strconv.Itoa(rand.Intn(1000000))
 	movies = append(movies, movie)
 	json.NewEncoder(w).Encode(movie)
 }
-func updateMovie(w http.ResponseWriter, r *http.Request) {
 
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.Id == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.Id = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+		}
+	}
 }
 
 func main() {
